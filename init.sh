a1d877055428755ae70af9fc05ada6b4995a0092
spikeekips@gmail.com <spikeekips@gmail.com@0cc5268d-5a44-0410-8fb3-519225a8c47b>
2008-08-27 20:46:36 +0000

    .

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ##################################################################
 
-. `dirname $0`/env.sh
+[ -f "`dirname $0`/env.sh" ] && . `dirname $0`/env.sh
 
 python `dirname $0`/init.py $1 $2
 
